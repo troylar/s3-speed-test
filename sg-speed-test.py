f6f2171567da158b346d8d4bb4d0955a82a458bb
Troy Larson <troylar@gmail.com>
2018-11-16 15:52:12 +0000

    Add wait_after_verify flag

--- sg-speed-test.py
+++ sg-speed-test.py
@@ -57,7 +57,7 @@ def copy_file(source, target, pipe_v, pipe, relative_path, thread_limiter):
         thread_limiter.release()
 
 
-def upload(pipe, pipe_v, folder, target, thread_limiter):
+def upload(pipe, pipe_v, folder, target, thread_limiter, verify):
     start = datetime.datetime.now()
     threads = []
     for root, dirs, files in os.walk(folder, topdown=True):
@@ -81,25 +81,34 @@ def upload(pipe, pipe_v, folder, target, thread_limiter):
     pipe_v.send('DONE')
 
 
-def write_report(report):
+def write_report(report, wait_for_verify):
     with open('report.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['File', 'Size in Bytes', 'S3 Latency in Ms'])
         for k in report.keys():
             row = report[k]
-            writer.writerow([k, row['size'], row['s3_latency']])
+            if wait_for_verify:
+                writer.writerow([k, row['size'], row['s3_latency']])
+            else:
+                writer.writerow([k, row['size']])
 
 
-def report(upload_pipe, verify_pipe, start_time):
+def report(upload_pipe, verify_pipe, start_time, wait_for_verify):
     report = {}
     upload_done = False
-    verify_done = False
+    verify_done = not wait_for_verify
     while True:
-        v = verify_pipe.recv()
+        if wait_for_verify:
+            v = verify_pipe.recv()
+            if v:
+                if v == 'DONE':
+                    print('Verify DONE')
+                    verify_done = True
+                else:
+                    if v[0] not in report:
+                        report[v[0]] = {}
+                    report[v[0]]['s3_done_time'] = v[1]
         u = upload_pipe.recv()
-        if v and v == 'DONE':
-            print('Verify DONE')
-            verify_done = True
         if u and u == 'DONE':
             print('Upload DONE')
             upload_done = True
@@ -110,19 +119,16 @@ def report(upload_pipe, verify_pipe, start_time):
                 report[u[0]] = {}
             report[u[0]]['size'] = u[1]
             report[u[0]]['copy_done_time'] = u[2]
-        if v:
-            if v[0] not in report:
-                report[v[0]] = {}
-            report[v[0]]['s3_done_time'] = v[1]
     total_size = 0
     for k in report:
-        report[k]['s3_latency'] = (report[k]['s3_done_time'] - report[k]['copy_done_time']).total_seconds() * 1000
+        if wait_for_verify:
+            report[k]['s3_latency'] = (report[k]['s3_done_time'] - report[k]['copy_done_time']).total_seconds() * 1000
         total_size = total_size + report[k]['size']
 
-    write_report(report)
+    write_report(report, wait_for_verify)
     time_in_secs = (datetime.datetime.now() - start_time).total_seconds()
-    print('File Count: {}'.format(len(report)))
     print('Total Size: {}'.format(humanfriendly.format_size(total_size)))
+    print('File Count: {}'.format(len(report)))
     print('Execution Time: {} second(s)'.format(time_in_secs))
     print('Performance: {}/sec'.format(humanfriendly.format_size((total_size / time_in_secs))))
 
@@ -132,24 +138,27 @@ def report(upload_pipe, verify_pipe, start_time):
 @click.argument('target_path')
 @click.argument('storage_gateway_bucket_name')
 @click.option('--max_threads', default=100, help='Maximum number of threads')
+@click.option('--wait_for_verify/--no_wait_for_verify', default=True, help='Wait for verify after copy')
 @click.option('--output_file', default='./report.csv', help='Output report file')
-def main(source_path, target_path, storage_gateway_bucket_name, output_file, max_threads):
+def main(source_path, target_path, storage_gateway_bucket_name, output_file, max_threads, wait_for_verify):
     thread_limiter = threading.BoundedSemaphore(max_threads)
     start = datetime.datetime.now()
     upload_pipe_v, verify_pipe_v = Pipe()
     verify_pipe, report_v_pipe = Pipe()
     upload_pipe, report_u_pipe = Pipe()
 
-    upload_p = Process(target=upload, args=(upload_pipe, upload_pipe_v, source_path, target_path, thread_limiter))
-    verify_p = Process(target=verify, args=(verify_pipe, verify_pipe_v, storage_gateway_bucket_name))
-    report_p = Process(target=report, args=(report_u_pipe, report_v_pipe, start))
+    upload_p = Process(target=upload, args=(upload_pipe, upload_pipe_v, source_path, target_path, thread_limiter, wait_for_verify))
+    if wait_for_verify:
+        verify_p = Process(target=verify, args=(verify_pipe, verify_pipe_v, storage_gateway_bucket_name))
+        verify_p.start()
+    report_p = Process(target=report, args=(report_u_pipe, report_v_pipe, start, wait_for_verify))
 
     report_p.start()
     upload_p.start()
-    verify_p.start()
-    report_p.join()
     upload_p.join()
-    verify_p.join()
+    if wait_for_verify:
+        verify_p.join()
+    report_p.join()
 
 
 if __name__ == '__main__':
